7f76e77a088bf3918ed5a123964136346094492c
Lmacconnell2 <mac23068@byui.edu>
2026-07-20 13:52:35 -0600

    rename

--- apps/api/docker/render-startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Starting API"
-exec node dist/server.js
-
-echo "Done."
--- apps/api/docker/startup.sh
+++ apps/api/docker/startup.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Running Better Auth migration..."
-node dist/database/migrations/runBetterAuthMigrations.js
-
-echo "Setting up database..."
-node dist/database/setup.js
-
-echo "Seeding Database"
-node dist/database/seed.js
-node dist/database/seedUser.js
-
 echo "Starting API"
 exec node dist/server.js
 
